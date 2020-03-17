cc4bb1bc7111916a2342411ca540e8d113a79807
nlilaramani <6720026+nlilaramani@users.noreply.github.com>
2020-03-16 22:43:17 -0400

    fixes

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ def get_hit_count():
           retries = 5
           while True:
                  try: return cache.incr('hits')
-                  except redis.exceptions.ConnectionError as exc:
+                 except redis.exceptions.ConnectionError as exc:
                           if retries == 0:
                                  raise exc
                           retries -= 1
                           time.sleep(0.5)
 @app.route('/')
 def hello():
-         count = get_hit_count()
-         return 'Hello World! I have been seen {} times.\n'.format(count)
+          count = get_hit_count()
+          return 'Hello World! I have been seen {} times.\n'.format(count)
 if __name__ == "__main__":
           app.run(host="0.0.0.0", debug=True)
