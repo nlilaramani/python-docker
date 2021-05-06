e8c9a618ea5f173e78220c54210355dab9c4ad53
nlilaramani <6720026+nlilaramani@users.noreply.github.com>
2021-05-06 18:16:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import time
 import redis
+import socket
 from flask import Flask
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
@@ -15,6 +16,6 @@ def get_hit_count():
 @app.route('/')
 def hello():
           count = get_hit_count()
-          return 'Hello World! I have been seen {} times.\n'.format(count)
+          return socket.gethostname()+'.V1>>Hello World! I have been seen {} times.\n'.format(count)
 if __name__ == "__main__":
           app.run(host="0.0.0.0", debug=True)
